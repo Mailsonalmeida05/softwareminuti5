22f2771a6567e51fd33a7650ad3b0ca760a9a202
Mailson Almeida Cerqueira <124589889+Mailsonalmeida05@users.noreply.github.com>
2023-07-21 10:18:03 -0300

    commit init

---
+++ bebidas.js
@@ -0,0 +1,17 @@
+let menuJson = [
+    {id:1, name:'PRODUTO R$ 9,90', img:'produtos.png', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'Descrição do Produto.'},
+    {id:2, name:'PRODUTO R$ 9,90', img:'produtos.png', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'Descrição do Produto.'},
+    {id:3, name:'PRODUTO R$ 12,00', img:'produtos.png', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'Descrição do Produto. '},
+    {id:4, name:'PRODUTO R$ 8,00', img:'produtos.png', price:[8.00, 8.00, 8.00], sizes:[' ', '',''], description:'Descrição do Produto. '},
+    {id:5, name:'PRODUTO R$ 8,90', img:'produtos.png', price:[8.90, 8.90, 8.90], sizes:[' ', '',''], description:'Descrição do Produto.'},
+    {id:6, name:'PRODUTO R$ 11,80', img:'produtos.png', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'Descrição do Produto. '},
+    {id:7, name:'PRODUTO R$ 18,90', img:'produtos.png', price:[18.90, 18.90, 18.90], sizes:[' ', '',''], description:'Descrição do Produto.'},
+    {id:8, name:'PRODUTO R$ 38,00', img:'produtos.png', price:[38.00, 38.00, 38.00], sizes:['','', ''], description:'Descrição do Produto.'},
+    {id:9, name:'PRODUTO R$2,00', img:'produtos.png', price:[2.00, 2.00, 2.00], sizes:['','', ''], description:'Descrição do Produto.'},
+    {id:10, name:'PRODUTO R$ 7,90 ', img:'produtos.png', price:[7.90, 7.90, 7.90], sizes:[' ', '',''], description:'Descrição do Produto.'},
+    {id:11, name:'PRODUTO R$ 10,90', img:'produtos.png', price:[10.90, 10.90, 10.90], sizes:[' ', '',''], description:'Descrição do Produto.'},
+    {id:12, name:'PRODUTO R$ 12,00', img:'produtos.png', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'Descrição do Produto. '},
+     {id:13, name:'PRODUTO R$ 12,00', img:'produtos.png', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'Descrição do Produto. '},
+ 
+   
+];
